f8eb50ad168f285a57543fc3be32f0d74bc3fb64
Othman Bella <147095415+Othman-Id-bella@users.noreply.github.com>
2023-11-16 00:22:00 +0100

    Add files via upload

---
+++ ex12.py
@@ -0,0 +1,9 @@
+A=int(input("enter the first number:"))
+B=int(input("enter the second number:"))
+
+if A*B>0:
+    print("the value of number A is:",B)
+    print("the value of number B is:",A)
+else:
+    print("the value of number A is:",A+B)
+    print("the value of number B is:",A*B)
